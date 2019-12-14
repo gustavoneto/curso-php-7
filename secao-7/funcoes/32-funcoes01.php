38e21f965dfb7a80aaeeb3fb9b98b9f77650183f
Gustavo Neto <gustavopcn@gmail.com>
2019-12-14 20:33:04 -0300

    anotação

--- secao-7/funcoes/32-funcoes01.php
+++ secao-7/funcoes/32-funcoes01.php
@@ -1,13 +1,15 @@
 <?php
 
-function ola($texto) {
+// Parametros que não tem valor padrão coloque sempre a esquerda para evitar erros.
+function ola($texto="mundo", $periodo="Bom dia") {
 
-    return "Olá $texto!<br>";
+    return "Olá $texto! $periodo! <br>";
 
 }
 
-echo ola("Mundo");
-echo ola("Gustavo");
-echo ola("João");
+echo ola(); // Se existem dois parâmetros e apenas 1 com valor padrão, é obrigado passar o que não tem valor
+echo ola("você", "Boa noite");
+echo ola("Gustavo", "Boa tarde");
+echo ola("João", "");
 
 ?>
